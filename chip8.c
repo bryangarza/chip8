2ce3b9a5d15db2f4c60b8efb8c53aa802fd0dc3a
Bryan Garza <brygarza@gmail.com>
2015-02-15 15:20:04 -0800

    Define variables for emu

--- chip8.c
+++ chip8.c
@@ -1,9 +1,16 @@
 int main(int argc, char *argv[])
 {
-  /* 35 two byte long opcodes */
-  unsigned short opcode;
+    unsigned short opcode;
+    unsigned char memory[4096];
+    unsigned char V[16];
+    unsigned short I;
+    unsigned short pc;
+    unsigned char gfx[64 * 32];
+    unsigned char delay_timer;
+    unsigned char sound_timer;
+    unsigned short stack[16];
+    unsigned short sp;
+    unsigned char key[16];
 
-  unsigned char memory[4096];
-
-  return(0);
+    return(0);
 }
