837b3c75a7a3227d74e01178a968971e96c00e90
Bryan Garza <brygarza@gmail.com>
2015-02-15 16:00:20 -0800

    Create a header file, read from binary ROM

--- chip8.c
+++ chip8.c
@@ -1,16 +1,36 @@
+#include <string.h>
+#include <stdio.h>
+
+#include "chip8.h"
+
+unsigned short opcode;
+unsigned char memory[4096];
+unsigned char V[16];
+unsigned short I;
+unsigned short pc;
+unsigned char gfx[64 * 32];
+unsigned char delay_timer;
+unsigned char sound_timer;
+unsigned short stack[16];
+unsigned short sp;
+unsigned char key[16];
+
 int main(int argc, char *argv[])
 {
-    unsigned short opcode;
-    unsigned char memory[4096];
-    unsigned char V[16];
-    unsigned short I;
-    unsigned short pc;
-    unsigned char gfx[64 * 32];
-    unsigned char delay_timer;
-    unsigned char sound_timer;
-    unsigned short stack[16];
-    unsigned short sp;
-    unsigned char key[16];
-
+    CPUReset();
     return(0);
+}
+
+void CPUReset()
+{
+    I = 0;
+    pc = 0x200;
+    memset(V, 0, sizeof(V));
+    FILE *in;
+    in = fopen("./c8games/PONG", "rb");
+    fread(&memory[0x200], 0xfff, 1, in);
+    /* for(int i = 0; i < (0xfff - 0x200); i++) { */
+    /*     printf("%x ", memory[i+0x200]); */
+    /* } */
+    fclose(in);
 }
