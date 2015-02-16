a5173cb22813fc22913dd49d44f77697b58ccdc2
Bryan Garza <brygarza@gmail.com>
2015-02-16 09:36:28 -0800

    Finish all but one instruction

--- chip8.c
+++ chip8.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "chip8.h"
 
@@ -14,10 +15,36 @@ unsigned char sound_timer;
 unsigned short stack[16];
 unsigned short sp;
 unsigned char key[16];
+unsigned char draw_flag;
+unsigned char chip8_fontset[80] =
+{
+  0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+  0x20, 0x60, 0x20, 0x20, 0x70, // 1
+  0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+  0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+  0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+  0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+  0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+  0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+  0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+  0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+  0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+  0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+  0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+  0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+  0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+  0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
 
 int main(int argc, char *argv[])
 {
     cpu_reset();
+
+    /* load fontset into memory */
+    for(int i = 0; i < 80; i++) {
+        memory[i] = chip8_fontset[i];
+    }
     return(0);
 }
 
@@ -53,6 +80,8 @@ void emulate_cycle()
     case 0x0000:
         switch(opcode & 0x000F) {
         case 0x0000: /* 0x00E0: Clears the screen */
+            memset(gfx, 0, sizeof(gfx));
+            pc += 2;
             break;
         case 0x000E: /* 0x00EE: Returns from a subroutine */
             pc = stack[sp];
@@ -133,12 +162,12 @@ void emulate_cycle()
         case 0x0005: /* 0x8XY5 VY is subtracted from VX. VF is set to 0 when
                       * there's a borrow, and to 1 when there isn't.
                       */
-            V[(opcode & 0x0F00) >> 8] -= V[(opcode & 0x00F0) >> 4];
             if (V[opcode & 0x0F00 >> 8] > V[(opcode & 0x00F0) >> 4]) {
                 V[0xF] = 1;
             }
             else {
                 V[0xF] = 0;
+            V[(opcode & 0x0F00) >> 8] -= V[(opcode & 0x00F0) >> 4];
             }
             pc += 2;
             break;
@@ -179,10 +208,15 @@ void emulate_cycle()
         }
         break;
     case 0xA000: /* 0xANNN Sets I to the address NNN */
+        I = opcode & 0x0FFF;
+        pc += 2;
         break;
     case 0xB000: /* 0xBNNN Jumps to the address NNN plus V0 */
+        pc = (opcode & 0x0FFF) + V[0];
         break;
-    case 0xC000: /* 0xCNNN Sets VX to a random number, masked by NN */
+    case 0xC000: /* 0xCXNN Sets VX to a random number, masked by NN */
+        V[(opcode & 0x0F00) >> 8] = rand() % 0xFFFF & (opcode & 0x00FF);
+        pc += 2;
         break;
     case 0xD000: /* 0xDXYN Sprites stored in memory at location in index
                   * register (I), maximum 8bits wide. Wraps around the
@@ -190,16 +224,50 @@ void emulate_cycle()
                   * 1 otherwise it is zero. All drawing is XOR drawing (i.e. it
                   * toggles the screen pixels).
                   */
+        {
+            unsigned short x = V[(opcode & 0x0F00) >> 8];
+            unsigned short y = V[(opcode & 0x00F0) >> 4];
+            unsigned short height = opcode & 0x000F;
+            unsigned short pixel;
+
+            V[0xF] = 0;
+            for (int yline = 0; yline < height; yline++) {
+                pixel = memory[I + yline];
+                for (int xline = 0; xline < 8; xline++) {
+                    if ((pixel & (0x80 >> xline)) != 0) {
+                        if (gfx[(x + xline + ((y + yline) * 64))] == 1) {
+                            V[0xF] = 1;
+                        }
+                        gfx[x + xline + ((y + yline) * 64)] ^= 1;
+                    }
+                }
+            }
+
+            draw_flag = 1;
+            pc += 2;
+        }
         break;
     case 0xE000:
-        switch(opcode & 0x000F) {
-        case 0x000E: /* 0xEX9E Skips the next instruction if the key stored in
+        switch(opcode & 0x00FF) {
+        case 0x009E: /* 0xEX9E Skips the next instruction if the key stored in
                       * VX is pressed.
                       */
+            if (key[V[(opcode & 0x0F00) >> 8]] != 0) {
+                pc += 4;
+            }
+            else {
+                pc += 2;
+            }
             break;
-        case 0x0001: /* 0xEXA1 Skips the next instruction if the key stored in
+        case 0x00A1: /* 0xEXA1 Skips the next instruction if the key stored in
                       * VX isn't pressed.
                       */
+            if (key[V[(opcode & 0x0F00) >> 8]] == 0) {
+                pc += 4;
+            }
+            else {
+                pc += 2;
+            }
             break;
         }
         break;
@@ -208,20 +276,29 @@ void emulate_cycle()
         case 0x0000:
             switch(opcode & 0x000F) {
             case 0x0007: /* 0xFX07 Sets VX to the value of the delay timer */
+                V[(opcode & 0x0F00) >> 8] = delay_timer;
+                pc += 2;
                 break;
             case 0x000A: /* 0xFX0A A key press is awaited, and then stored in
                           * VX
                           */
+                /* TODO: fill this in, needs SDL instruction as well */
                 break;
             }
             break;
         case 0x0010:
             switch(opcode & 0x000F) {
             case 0x0005: /* 0xFX15 Sets the delay timer to VX */
+                delay_timer = V[(opcode & 0x0F00) >> 8];
+                pc += 2;
                 break;
             case 0x0008: /* 0xFX18 Sets the sound timer to VX */
+                sound_timer = V[(opcode & 0x0F00) >> 8];
+                pc += 2;
                 break;
             case 0x000E: /* 0xFX1E Adds VX to I */
+                I += V[(opcode & 0x0F00) >> 8];
+                pc += 2;
                 break;
             }
             break;
@@ -229,18 +306,45 @@ void emulate_cycle()
                       * character in VX. Characters 0-F (in hex) are represented
                       * by a 4x5 font.
                       */
+            I = V[(opcode & 0x0F00) >> 8] * 5;
             break;
         case 0x0030: /* 0xFX33 Stores the Binary-coded decimal representation of
                       * VX, with the most significant of the three digits at the
                       * address in I, the middle digit at I plus 1, and the
                       * least significant digit at I plus 2.
                       */
+            {
+                unsigned char vx = V[(opcode * 0x0F00) >> 8];
+                memory[I] = vx / 100;
+                memory[I + 1] = (vx / 10) % 10;
+                memory[I + 2] = (vx % 100) % 10;
+                pc += 2;
+            }
             break;
         case 0x0050: /* 0xFX55 Stores V0 to VX in memory starting at address I */
+            {
+                int vx = (opcode & 0x0F00) >> 8;
+                int v = 0;
+                while (v <= vx) {
+                    memory[I + v] = V[v];
+                }
+                I = I + vx + 1;
+                pc += 2;
+            }
             break;
         case 0x0060: /* Fills V0 to VX with values from memory starting at
                       * address I
                       */
+            {
+                int vx = (opcode & 0x0F00) >> 8;
+                int v = 0;
+                while (v <= vx) {
+                    V[v] = memory[I + v];
+                    v++;
+                }
+                I = I + vx + 1;
+                pc += 2;
+            }
             break;
         }
         break;
