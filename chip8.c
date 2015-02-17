23aa13a1377a3eef969e672d7280bddce333c9ec
Bryan Garza <brygarza@gmail.com>
2015-02-17 11:05:09 -0800

    Don't skip instructions
    I think that instructions were being skipped, comparing the hexdump of
    the ROM vs the print statements of the program shows that. I removed all
    the `pc += 2` in the switch statements because the program counter is
    already increasing in the get_next_opcode() function.

--- chip8.c
+++ chip8.c
@@ -162,7 +162,6 @@ void emulate_cycle()
         switch(opcode & 0x000F) {
         case 0x0000: /* 0x00E0: Clears the screen */
             memset(gfx, 0, sizeof(gfx));
-            pc += 2;
             break;
         case 0x000E: /* 0x00EE: Returns from a subroutine */
             pc = stack[sp];
@@ -180,53 +179,38 @@ void emulate_cycle()
         break;
     case 0x3000: /* 0x3XNN Skips the next instruction if VX equals NN */
         if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF)) {
-            pc += 4;
-        }
-        else {
             pc += 2;
         }
         break;
     case 0x4000: /* 0x4XNN Skips the next instruction if VX doesn't equal NN */
         if (V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF)) {
-            pc += 4;
-        }
-        else {
             pc += 2;
         }
         break;
     case 0x5000: /* 0x5XY0 Skips the next instruction if VX equals VY */
         if (V[(opcode & 0x0F00) >> 8] == (V[(opcode & 0x00F0) >> 4])) {
-            pc += 4;
-        }
-        else {
             pc += 2;
         }
         break;
     case 0x6000: /* 0x6XNN Sets VX to NN */
         V[(opcode & 0x0F00) >> 8] = (opcode * 0x00FF);
-        pc += 2;
         break;
     case 0x7000: /* 0x7XNN Adds NN to VX */
         V[(opcode & 0x0F00) >> 8] += (opcode * 0x00FF);
-        pc += 2;
         break;
     case 0x8000:
         switch(opcode & 0x000F) {
         case 0x0000: /* 0x8XY0 Sets VX to the value of VY */
             V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
-            pc += 2;
             break;
         case 0x0001: /* 0x8XY1 Sets VX to VX or VY */
             V[(opcode & 0x0F00) >> 8] |= V[(opcode & 0x00F0) >> 4];
-            pc += 2;
             break;
         case 0x0002: /* 0x8XY2 Sets VX to VX and VY */
             V[(opcode & 0x0F00) >> 8] &= V[(opcode & 0x00F0) >> 4];
-            pc += 2;
             break;
         case 0x0003: /* 0x8XY3 Sets VX to VX xor VY */
             V[(opcode & 0x0F00) >> 8] ^= V[(opcode & 0x00F0) >> 4];
-            pc += 2;
             break;
         case 0x0004: /* 0x8XY4 Adds VY to VX. VF is set to 1 when there's a
                       * carry, and to 0 when there isn't.
@@ -238,7 +222,6 @@ void emulate_cycle()
             }
 
             V[(opcode & 0x0F00) >> 8] += V[(opcode & 0x00F0) >> 4];
-            pc += 2;
             break;
         case 0x0005: /* 0x8XY5 VY is subtracted from VX. VF is set to 0 when
                       * there's a borrow, and to 1 when there isn't.
@@ -250,14 +233,12 @@ void emulate_cycle()
             }
 
             V[(opcode & 0x0F00) >> 8] -= V[(opcode & 0x00F0) >> 4];
-            pc += 2;
             break;
         case 0x0006: /* 0x8XY6 Shifts VX right by one. VF is set to the value of
                       * the least significant bit of VX before the shift.
                       */
             V[0xF] = V[(opcode & 0x0F00) >> 8] & 0x1;
             V[(opcode & 0x0F00) >> 8] >>= 1;
-            pc += 2;
             break;
         case 0x0007: /* 0x8XY7 Sets VX to VY minus VX. VF is set to 0 when
                       * there's a borrow, and 1 when there isn't.
@@ -269,35 +250,28 @@ void emulate_cycle()
             }
 
             V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4] - V[(opcode & 0x0F00) >> 8];
-            pc += 2;
             break;
         case 0x000E: /* 0x8XYE Shifts VX left by one. VF is set to the value of
                       * the most significant bit of VX before the shift.
                       */
             V[0xF] = V[(opcode & 0x0F00) >> 8] >> 7;
             V[(opcode & 0x0F00) >> 8] <<= 1;
-            pc += 2;
             break;
         }
         break;
     case 0x9000: /* 0x9XY0 Skips the next instruction if VX doesn't equal VY */
         if (V[(opcode & 0x0F000) >> 8] != V[(opcode & 0x00F0) >> 4]) {
-            pc += 4;
-        }
-        else {
             pc += 2;
         }
         break;
     case 0xA000: /* 0xANNN Sets I to the address NNN */
         I = opcode & 0x0FFF;
-        pc += 2;
         break;
     case 0xB000: /* 0xBNNN Jumps to the address NNN plus V0 */
         pc = V[0] + (opcode & 0x0FFF);
         break;
     case 0xC000: /* 0xCXNN Sets VX to a random number, masked by NN */
         V[(opcode & 0x0F00) >> 8] = rand() & (opcode & 0x00FF);
-        pc += 2;
         break;
     case 0xD000: /* 0xDXYN Sprites stored in memory at location in index
                   * register (I), maximum 8bits wide. Wraps around the
@@ -322,7 +296,6 @@ void emulate_cycle()
                     }
                 }
             }
-            pc += 2;
         }
         break;
     case 0xE000:
@@ -331,9 +304,6 @@ void emulate_cycle()
                       * VX is pressed.
                       */
             if (key[V[(opcode & 0x0F00) >> 8]] == 1) {
-                pc += 4;
-            }
-            else {
                 pc += 2;
             }
             break;
@@ -341,9 +311,6 @@ void emulate_cycle()
                       * VX isn't pressed.
                       */
             if (key[V[(opcode & 0x0F00) >> 8]] == 0) {
-                pc += 4;
-            }
-            else {
                 pc += 2;
             }
             break;
@@ -355,7 +322,6 @@ void emulate_cycle()
             switch(opcode & 0x000F) {
             case 0x0007: /* 0xFX07 Sets VX to the value of the delay timer */
                 V[(opcode & 0x0F00) >> 8] = delay_timer;
-                pc += 2;
                 break;
             case 0x000A: /* 0xFX0A A key press is awaited, and then stored in
                           * VX
@@ -368,15 +334,12 @@ void emulate_cycle()
             switch(opcode & 0x000F) {
             case 0x0005: /* 0xFX15 Sets the delay timer to VX */
                 delay_timer = V[(opcode & 0x0F00) >> 8];
-                pc += 2;
                 break;
             case 0x0008: /* 0xFX18 Sets the sound timer to VX */
                 sound_timer = V[(opcode & 0x0F00) >> 8];
-                pc += 2;
                 break;
             case 0x000E: /* 0xFX1E Adds VX to I */
                 I += V[(opcode & 0x0F00) >> 8];
-                pc += 2;
                 break;
             }
             break;
@@ -396,7 +359,6 @@ void emulate_cycle()
                 memory[I] = vx / 100;
                 memory[I + 1] = (vx / 10) % 10;
                 memory[I + 2] = vx % 10;
-                pc += 2;
             }
             break;
         case 0x0050: /* 0xFX55 Stores V0 to VX in memory starting at address I */
@@ -409,7 +371,6 @@ void emulate_cycle()
                 }
 
                 I = I + vx + 1;
-                pc += 2;
             }
             break;
         case 0x0060: /* Fills V0 to VX with values from memory starting at
@@ -425,7 +386,6 @@ void emulate_cycle()
                 }
 
                 I = I + vx + 1;
-                pc += 2;
             }
             break;
         }
